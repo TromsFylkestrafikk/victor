3b77359ee82a221d497f6e107bf27ded78d7e00f
Kåre Slettnes <kaare@slettnes.org>
2025-03-12 00:00:53 +0100

    Added tools from tilemaker

---
+++ tilemaker/get-landcover.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+mkdir -p landcover
+pushd landcover
+
+if ! [ -f "ne_10m_antarctic_ice_shelves_polys.zip" ]; then
+    curl --proto '=https' --tlsv1.3 -sSfO https://naciscdn.org/naturalearth/10m/physical/ne_10m_antarctic_ice_shelves_polys.zip
+    mkdir -p ne_10m_antarctic_ice_shelves_polys
+    unzip -o ne_10m_antarctic_ice_shelves_polys.zip -d ne_10m_antarctic_ice_shelves_polys
+fi
+
+if ! [ -f "ne_10m_urban_areas.zip" ]; then
+    curl --proto '=https' --tlsv1.3 -sSfO https://naciscdn.org/naturalearth/10m/cultural/ne_10m_urban_areas.zip
+    mkdir -p ne_10m_urban_areas
+    unzip -o ne_10m_urban_areas.zip -d ne_10m_urban_areas
+fi
+
+if ! [ -f "ne_10m_glaciated_areas.zip" ]; then
+    curl --proto '=https' --tlsv1.3 -sSfO https://naciscdn.org/naturalearth/10m/physical/ne_10m_glaciated_areas.zip
+    mkdir -p ne_10m_glaciated_areas
+    unzip -o ne_10m_glaciated_areas.zip -d ne_10m_glaciated_areas
+fi
+
+popd
