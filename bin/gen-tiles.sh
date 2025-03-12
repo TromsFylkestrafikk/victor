b39505adec82b653b7c0d9c46e1e414ef69086ed
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-03-12 15:23:30 +0100

    Added options to gen-tiles. URL is param. Outout is not yet implemented

--- bin/gen-tiles.sh
+++ bin/gen-tiles.sh
@@ -1,8 +1,43 @@
 #!/bin/bash
 
 DIR_VICTOR=$(realpath $(dirname $(dirname $0)))
-
 PBF_URL=https://download.geofabrik.de/europe/norway-latest.osm.pbf
+
+function usage {
+    echo "$(basename $0) [OPTIONS] [PBF_URL]
+
+This is a simple frontend to tilemaker for generating openmaptiles compatible
+mbtiles files based on regional data.
+
+PBF_URL is a url containing a osm.pbf dump, usually from geofabrik.de.
+Defaults is $PBF_URL
+
+OPTIONS
+    -h          This help
+    -o OUTPUT   Destination of .mbtiles file. Default destination is in the
+                tiles folder.
+"
+}
+
+PBF=""
+while getopts "ho:" option; do
+    case $option in
+        h) usage
+           exit
+           ;;
+        o) PBF=$OPTARG
+           ;;
+        *) usage
+           exit
+           ;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+
+if [[ ! -z $1 ]]; then
+    PBF_URL=$1
+fi
 
 if [[ ${PBF_URL: -8} != '.osm.pbf' ]]; then
     echo "Wrong file extension in downloaded chunk. Expected '.osm.pbf'" > /dev/stderr
@@ -10,8 +45,12 @@ if [[ ${PBF_URL: -8} != '.osm.pbf' ]]; then
 fi
 
 AREA=$(basename ${PBF_URL##*/} .osm.pbf)
-PBF=$DIR_VICTOR/tilemaker/osm/$AREA.osm.pbf
+if [[ $PBF = "" ]]; then
+    PBF=$DIR_VICTOR/tilemaker/osm/$AREA.osm.pbf
+fi
 MBTILES=$DIR_VICTOR/tiles/$AREA.mbtiles
+echo $PBF
+exit
 
 function download {
     pushd $DIR_VICTOR/tilemaker > /dev/null
@@ -47,13 +86,12 @@ function gen_tiles {
     fi
     rm -f $MBTILES
     resources=$DIR_VICTOR/tilemaker/resources
-    tilemaker --input $PBF \
-              --output $MBTILES \
+    tilemaker --output $MBTILES \
               --bbox -180,-85,180,85 \
               --config $resources/config-coastline.json \
               --process $resources/process-coastline.lua
     echo "done"
-
+    return
     echo -n "Adding $AREA to tiles ..."
     tilemaker --input $PBF \
               --output $MBTILES \
