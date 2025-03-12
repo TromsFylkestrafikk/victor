ae30b25aff731ff56eb6ebe8d19b69e3231804c2
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-03-12 11:37:38 +0100

    Process script for coastlines

--- bin/gen-tiles.sh
+++ bin/gen-tiles.sh
@@ -10,27 +10,36 @@ if [[ ${PBF_URL: -8} != '.osm.pbf' ]]; then
 fi
 
 AREA=$(basename ${PBF_URL##*/} .osm.pbf)
-
-cd $DIR_VICTOR/tilemaker
-echo -n "Downloading coastlines ... "
-./get-coastline.sh
-echo "OK"
-echo -n "Downloading landcover ... "
-./get-landcover.sh
-echo "OK"
-
-mkdir -p $DIR_VICTOR/tilemaker/osm
 PBF=$DIR_VICTOR/tilemaker/osm/$AREA.osm.pbf
 
-echo -n "Downloading OSM data for $AREA "
-if [ ! -f $PBF -o $(($(date +%s) - $(date -r $PBF +%s))) -gt 86000 ]; then
-    #curl -Ssf --output $PBF $PBF_URL
-    echo -n " ... "
-fi
-echo "OK"
+function download {
+    pushd $DIR_VICTOR/tilemaker > /dev/null
+    echo -n "Downloading coastlines ... "
+    ./get-coastline.sh
+    echo "OK"
+    echo -n "Downloading landcover ... "
+    ./get-landcover.sh
+    echo "OK"
 
-if [[ ! -f $PBF ]]; then
-    echo "Download of OSM data from '$PBF_URL' to destination '$PBF' failed" > /dev/stderr
-    exit
-fi
+    mkdir -p $DIR_VICTOR/tilemaker/osm
 
+    echo -n "Downloading OSM data for $AREA "
+    if [ ! -f $PBF -o $(($(date +%s) - $(date -r $PBF +%s))) -gt 86000 ]; then
+        #curl -Ssf --output $PBF $PBF_URL
+        echo -n " ... "
+    fi
+    echo "OK"
+
+    if [[ ! -f $PBF ]]; then
+        echo "Download of OSM data from '$PBF_URL' to destination '$PBF' failed" > /dev/stderr
+        exit
+    fi
+    popd > /dev/null
+}
+
+function gen_tiles {
+    echo -n "Generating world-wide coastlines and landcover ..."
+}
+
+download
+gen_tiles
