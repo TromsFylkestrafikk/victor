896d66d278c427225917134ff8ebd74a31731ee0
Kåre Slettnes <kaare.slettnes@tromsfylke.no>
2025-03-13 13:17:31 +0100

    Removed duplicate processing in openmaptiles tile setup.

--- tilemaker/resources/process-openmaptiles.lua
+++ tilemaker/resources/process-openmaptiles.lua
@@ -727,21 +727,6 @@ function way_function()
 	end
 end
 
--- Remap coastlines
-function attribute_function(attr,layer)
-	if attr["featurecla"]=="Glaciated areas" then
-		return { subclass="glacier" }
-	elseif attr["featurecla"]=="Antarctic Ice Shelf" then
-		return { subclass="ice_shelf" }
-	elseif attr["featurecla"]=="Urban area" then
-		return { class="residential" }
-	elseif layer=="ocean" then
-		return { class="ocean" }
-	else
-		return attr
-	end
-end
-
 -- ==========================================================
 -- Common functions
 
